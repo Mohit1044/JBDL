4620f67544428c7e1914e877501f4e71464df82d
LENOVO <LENOVO@DESKTOP-R4GPDA6>
2023-04-13 18:34:39 +0530

    Commit

---
+++ src/Mutilevel/Employee.java
@@ -0,0 +1,33 @@
+package Mutilevel;
+
+public class Employee {
+	String name;
+	int id;
+	String email;	
+	
+	public Employee() {
+		System.out.println("Employee Default Constructor");
+		// TODO Auto-generated constructor stub
+	}
+
+	public void display() {
+		System.out.println("Display from Employee");
+		System.out.println("Name: "+name+" id:"+id+" email:"+email);
+	}
+
+	public void updateSalary() {
+		System.out.println("updateSalary called");
+	}
+
+	public Employee(String name, int id, String email) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", id=" + id + ", email=" + email + "]";
+	}
+}
---
+++ src/Mutilevel/MultilevelInheritanceMain.java
@@ -0,0 +1,14 @@
+package Mutilevel;
+
+public class MultilevelInheritanceMain {
+
+public static void main(String[] args) {
+		
+		
+		FulltimeEmployee fte = new FulltimeEmployee("ABC", 1, "a@a.com", 100, "ABC");
+		
+		fte.display();
+
+
+	}
+}
